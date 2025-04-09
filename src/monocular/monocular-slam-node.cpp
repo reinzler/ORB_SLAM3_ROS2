1404c50ecdf2f9f1a199d0441ddc44c7e99bed16
reinzler <lzlzdflz@gmail.com>
2025-04-09 16:44:30 +0300

    Colcon Build Error fixed (changed cv_bridge/cv_bridge.h to cv_bridge/cv_bridge.hpp)

--- src/monocular/monocular-slam-node.cpp
+++ src/monocular/monocular-slam-node.cpp
@@ -1,6 +1,8 @@
 #include "monocular-slam-node.hpp"
-
-#include<opencv2/core/core.hpp>
+#include <opencv2/core/core.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <Eigen/Geometry>
+#include <sophus/se3.hpp> // Include Sophus for SE3 type
 
 using std::placeholders::_1;
 
@@ -8,11 +10,14 @@ MonocularSlamNode::MonocularSlamNode(ORB_SLAM3::System* pSLAM)
 :   Node("ORB_SLAM3_ROS2")
 {
     m_SLAM = pSLAM;
-    // std::cout << "slam changed" << std::endl;
     m_image_subscriber = this->create_subscription<ImageMsg>(
         "camera",
         10,
         std::bind(&MonocularSlamNode::GrabImage, this, std::placeholders::_1));
+
+    // Create a publisher for the pose messages
+    m_pose_publisher = this->create_publisher<geometry_msgs::msg::PoseStamped>("camera_pose", 10);
+
     std::cout << "slam changed" << std::endl;
 }
 
@@ -38,6 +43,28 @@ void MonocularSlamNode::GrabImage(const ImageMsg::SharedPtr msg)
         return;
     }
 
-    std::cout<<"one frame has been sent"<<std::endl;
-    m_SLAM->TrackMonocular(m_cvImPtr->image, Utility::StampToSec(msg->header.stamp));
+    std::cout << "one frame has been sent" << std::endl;
+
+    // Track monocular frame and get camera pose
+    Sophus::SE3f Tcw = m_SLAM->TrackMonocular(m_cvImPtr->image, Utility::StampToSec(msg->header.stamp));
+
+    // Prepare pose message
+    geometry_msgs::msg::PoseStamped pose_msg;
+    pose_msg.header.stamp = this->get_clock()->now();  // Timestamp the message
+    pose_msg.header.frame_id = "camera_frame";  // Reference frame for the pose
+
+    // Set the position from the translation part of the SE3 pose
+    pose_msg.pose.position.x = Tcw.translation().x();
+    pose_msg.pose.position.y = Tcw.translation().y();
+    pose_msg.pose.position.z = Tcw.translation().z();
+    
+    // Convert rotation matrix to quaternion and set the orientation part
+    Eigen::Quaternionf q(Tcw.rotationMatrix());
+    pose_msg.pose.orientation.x = q.x();
+    pose_msg.pose.orientation.y = q.y();
+    pose_msg.pose.orientation.z = q.z();
+    pose_msg.pose.orientation.w = q.w();
+
+    // Publish the pose message
+    m_pose_publisher->publish(pose_msg);
 }
--- src/monocular/monocular-slam-node.hpp
+++ src/monocular/monocular-slam-node.hpp
@@ -3,15 +3,14 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
-
-#include <cv_bridge/cv_bridge.h>
-
+#include <cv_bridge/cv_bridge.hpp>
 #include "System.h"
 #include "Frame.h"
 #include "Map.h"
 #include "Tracking.h"
-
 #include "utility.hpp"
+
+#include <geometry_msgs/msg/pose_stamped.hpp>  // Add this for PoseStamped
 
 class MonocularSlamNode : public rclcpp::Node
 {
@@ -30,6 +29,11 @@ private:
     cv_bridge::CvImagePtr m_cvImPtr;
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_image_subscriber;
+
+    // Add publisher for PoseStamped messages
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr m_pose_publisher;  // New publisher for Pose
+
 };
 
 #endif
+
